84130ab190688d104ed7e302ab2566c6f0c3e1bc
Smit Roy <smitroy22@gmail.com>
2026-04-05 13:09:34 +0530

    Implemented JWT generation.

--- src/main/java/com/smit/springSecurity/entity/User.java
+++ src/main/java/com/smit/springSecurity/entity/User.java
@@ -21,7 +21,7 @@ public class User implements UserDetails {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String name;
+//    private String name;
 
     @Column(unique = true)
     private String email;
--- src/test/java/com/smit/springSecurity/SpringSecurityApplicationTests.java
+++ src/test/java/com/smit/springSecurity/SpringSecurityApplicationTests.java
@@ -1,13 +1,31 @@
 package com.smit.springSecurity;
 
+import com.smit.springSecurity.entity.User;
+import com.smit.springSecurity.service.impl.JwtService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringSecurityApplicationTests {
 
+
+    @Autowired
+    private JwtService jwtService;
+
 	@Test
 	void contextLoads() {
+
+        User user = new User(4L, "smit@email.com", "smit@1234");
+
+        String token = jwtService.generateToken(user);
+
+        System.out.println("Generated Token: " + token);
+
+        Long idFromToken = jwtService.getUserIdFromToken(token);
+
+        System.out.println("User ID from Token: " + idFromToken);
+
 	}
 
 }
