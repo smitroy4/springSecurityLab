1a519f453456d5a41281255fdb3573af69bf4205
Smit Roy <smitroy22@gmail.com>
2026-04-12 23:41:00 +0530

    Flushed all config and classes for restart.

--- src/test/java/com/smit/springSecurity/SpringSecurityApplicationTests.java
+++ src/test/java/com/smit/springSecurity/SpringSecurityApplicationTests.java
@@ -1,7 +1,5 @@
 package com.smit.springSecurity;
 
-import com.smit.springSecurity.entity.User;
-import com.smit.springSecurity.service.impl.JwtService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
