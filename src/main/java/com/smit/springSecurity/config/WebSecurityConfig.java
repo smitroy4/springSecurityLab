f17b58cf47cecba2c06127a89cdcd61b8b244d53
Smit Roy <smitroy22@gmail.com>
2026-04-22 07:09:17 +0530

    Configured WebSecurityConfig: 1. Authorized all routes 2. Permitted /api/laptops 3. Authorized api/laptops/** with ADMIN 4. Disabled CSRF 5. Disabled Session Creation (STATELESS) 6. Created USERS - Admin, Smit 7. Initialized PasswordEncoder(BCrypt)

--- src/main/java/com/smit/springSecurity/config/WebSecurityConfig.java
+++ src/main/java/com/smit/springSecurity/config/WebSecurityConfig.java
@@ -5,6 +5,14 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -15,10 +23,43 @@ public class WebSecurityConfig {
     SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
 
         httpSecurity
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/laptops").permitAll()
+                        .requestMatchers("/laptops/**").hasAnyRole("ADMIN")
+                        .anyRequest().authenticated()
+                )
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(httpSecuritySessionManagementConfigurer
+                        -> httpSecuritySessionManagementConfigurer
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .formLogin(Customizer.withDefaults());
 
         return httpSecurity.build();
 
+    }
+
+
+    @Bean
+    UserDetailsService InMemoryuserDetailsService(){
+        UserDetails normalUser = User
+                .withUsername("smit")
+                .password(passwordEncoder().encode("Smit@1234"))
+                .roles("USER")
+                .build();
+
+        UserDetails adminUser = User
+                .withUsername("admin")
+                .password(passwordEncoder().encode("Admin@1234"))
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(normalUser, adminUser);
+
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 
 }
